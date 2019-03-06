507f6835f32af3173faad4cf001b3248d2d94489
UrbanRat <deadrat1@ya.ru>
2019-03-06 16:38:24 +0300

    Add examples

---
+++ examples/readme.txt
@@ -0,0 +1,7 @@
+Перейти в директорию с примером.
+
+Файлы шаблонов и content.json находятся в субдиректориях /templates/bakeproject/build/html/
+
+Запустить grunt в директории develop
+Внести изменения в файле content.json - изменить "cur_section"
+Файлы .html будут собраны в директории /templates/bakeproject/
---
+++ examples/three/develop/Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+	require('jit-grunt')(grunt);
+
+	template = '../templates/bakeproject/';
+	content = template + 'build/html/content.json'; // multilingual content
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		bake: {
+			main: {
+				options: {
+					content: content,
+				//	section: "en",
+					section: function(){
+						var cont = grunt.file.readJSON(content);
+						var lang = cont.cur_section !== undefined ? cont.cur_section : "en";
+
+						return lang;
+					},
+					addPath: "section", //"section" - special string, section==options.section
+				},
+				files: [
+					{
+						cwd: template + 'build/html/',
+						src: ['.html', '! _*.html'],
+						dest: template,
+						expand: true,
+					},
+				]
+			},
+		},
+
+		watch: {
+			html: {
+				files: [template + 'build/html/*.html', template + 'build/html/content.json'],
+				tasks: ['bake:main'],
+				options: {
+					spawn: false,
+					event: ['added', 'changed'],
+				},
+			},
+		},
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-bake');
+
+	grunt.registerTask('dev', ['watch']);
+	grunt.registerTask('ba', ['bake:main',]);
+	grunt.registerTask('default', 'dev');
+};
